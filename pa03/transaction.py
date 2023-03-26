157bd64465c234034cbe1c0b20a5582175219a44
zhovatter <123821138+zhovatter@users.noreply.github.com>
2023-03-26 18:06:54 -0400

    Update transaction.py

---
+++ pa03/transaction.py
@@ -0,0 +1,50 @@
+'''
+
+'''
+import sqlite3
+import os
+
+def toDict(t):
+    ''' t is a tuple (rowid,title, desc,completed)'''
+    print('t='+str(t))
+    todo = {'rowid':t[0], 'title':t[1], 'desc':t[2], 'completed':t[3]}
+    return todo
+
+class TodoList():
+    def __init__(self):
+        self.runQuery('''CREATE TABLE IF NOT EXISTS todo
+                    (title text, desc text, completed int)''',())
+    
+    def selectActive(self):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=0",())
+
+    def selectAll(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo",())
+
+    def selectCompleted(self):
+        ''' return all of the completed tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=1",())
+
+    def add(self,item):
+        ''' create a todo item and add it to the todo table '''
+        return self.runQuery("INSERT INTO todo VALUES(?,?,?)",(item['title'],item['desc'],item['completed']))
+
+    def delete(self,rowid):
+        ''' delete a todo item '''
+        return self.runQuery("DELETE FROM todo WHERE rowid=(?)",(rowid,))
+
+    def setComplete(self,rowid):
+        ''' mark a todo item as completed '''
+        return self.runQuery("UPDATE todo SET completed=1 WHERE rowid=(?)",(rowid,))
+
+    def runQuery(self,query,tuple):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        con= sqlite3.connect(os.getenv('HOME')+'/todo.db')
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
