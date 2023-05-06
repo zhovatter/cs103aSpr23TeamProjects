a2271b5931bdb3afd2b2ed5916fc41385799d80c
Christinalxy29 <123264993+Christinalxy29@users.noreply.github.com>
2023-05-06 00:04:18 -0400

    hi

--- ca02/firstapp/routes/christina.js
+++ ca02/firstapp/routes/christina.js
@@ -6,6 +6,16 @@ router.get('/christina/',
     res.render('christina');
 });
 
+const axios = require('axios')
+router.post('/christina/openai',
+async (req,res,next) => {
+res.locals.promot = req.body.prompt
+response =
+await axios.post('http://gracehopper.cs-i.brandeis.edu:3500/openai',
+{prompt:"Generate a joke about" + req.body.prompt})
+res.json(response.data.choices[0].message.content)
+})
+
 router.post('/christina',
   async (req, res, next) => {
       res.redirect('/christina')
